ccbb2b32ce3103485f760d078232f154a74af535
ssohee <jsohee98@naver.com>
2023-03-21 14:20:14 +0900

    fix: creatUser에 URI도 함께 생성

--- src/main/java/com/example/restservice/user/UserController.java
+++ src/main/java/com/example/restservice/user/UserController.java
@@ -2,8 +2,11 @@ package com.example.restservice.user;
 
 import com.example.restservice.helloworld.HelloWorldBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,8 +30,17 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity createUser(@RequestBody User user) {
         //JSON 형태를 자바 객체 형태로 바꿔주기 위해 @RequestBody
         User savedUser = service.saveUser(user);
+
+        //새로 추가 URI (/users/{id})
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        //location (URI) 정보 전달
+        return ResponseEntity.created(location).build();
     }
 }
