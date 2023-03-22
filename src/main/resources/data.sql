7332278304fda37985681a42023c4cefe33c2cba
ssohee <jsohee98@naver.com>
2023-03-22 10:56:06 +0900

    fix: yml파일에 sql설정 추가/ data.sql 파일에 데이터 insert

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1, sysdate, 'ssohee', '1234', '981221');
+insert into user values(2, sysdate, 'ssohee2', '1234', '981221');
+insert into user values(3, sysdate, 'ssohee3', '1234', '981221');
